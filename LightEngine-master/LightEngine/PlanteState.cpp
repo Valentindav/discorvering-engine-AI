7da89147dbc908dac0ef2fa7050cd0bdccdf5a83
Valentindav <vdavid@gaming.tech>
2025-02-13 15:16:46 +0100

    d

--- LightEngine-master/LightEngine/PlanteState.cpp
+++ LightEngine-master/LightEngine/PlanteState.cpp
@@ -1,1 +1,43 @@
 #include "PlanteState.h"
+
+void IdleState::Start(PlantEntity* plant)
+{
+	std::cout << "Plante en mode Idle" << std::endl;
+}
+
+void IdleState::Update(PlantEntity* plant, float dt)
+{
+	if (plant->IsZombieInRange()) {
+		plant->SetState(new ShootingState());
+	}
+	else {
+		plant->SetState(new ReloadState());
+	}
+}
+
+void ReloadState::Start(PlantEntity* plant)
+{
+	std::cout << "Rechargement..." << std::endl;
+}
+
+void ReloadState::Update(PlantEntity* plant, float dt)
+{
+	plant->SetAmmo(5);
+	plant->SetState(new IdleState());
+}
+
+void ShootingState::Start(PlantEntity* plant)
+{
+	std::cout << "Plante tire !" << std::endl;
+}
+
+void ShootingState::Update(PlantEntity* plant, float dt)
+{
+	std::cout << plant->GetAmmo();
+	if (plant->GetAmmo() > 0) {
+		plant->Shoot();
+	}
+	else {
+		plant->SetState(new ReloadState());
+	}
+}
--- LightEngine-master/LightEngine/PlanteState.h
+++ LightEngine-master/LightEngine/PlanteState.h
@@ -10,61 +10,26 @@ public:
 	virtual void Update(PlantEntity* plant, float dt) = 0;
 };
 
-class ShootingState;
-class ReloadState;
 class IdleState : public PlanteState
 {
 public:
-	void Start(PlantEntity* plant) override
-	{
-		std::cout << "Plante en mode Idle" << std::endl;
-	}
+	void Start(PlantEntity* plant) override;
 
-	void Update(PlantEntity* plant, float dt) override
-	{
-		if (plant->IsZombieInRange()) {
-			plant->SetState(new ShootingState());
-		}
-		else {
-			plant->SetState(new ReloadState());
-		}
-	}
+	void Update(PlantEntity* plant, float dt) override;
 };
 
 class ReloadState : public PlanteState
 {
 public:
-	void Start(PlantEntity* plant) override
-	{
-		std::cout << "Rechargement..." << std::endl;
-	}
+	void Start(PlantEntity* plant) override;
 
-	void Update(PlantEntity* plant, float dt) override
-	{
-		plant->SetAmmo(5);
-		plant->SetState(new IdleState());
-	}
+	void Update(PlantEntity* plant, float dt) override;
 };
 
 class ShootingState : public PlanteState
 {
 public:
-	void Start(PlantEntity* plant) override
-	{
-		std::cout << "Plante tire !" << std::endl;
-	}
+	void Start(PlantEntity* plant) override;
 
-	void Update(PlantEntity* plant, float dt) override
-	{
-		std::cout << plant->GetAmmo();
-		if (plant->GetAmmo() > 0) {
-			plant->Shoot();
-		}
-		else {
-			plant->SetState(new ReloadState());
-		}
-	}
+	void Update(PlantEntity* plant, float dt) override;
 };	
-
-
-
